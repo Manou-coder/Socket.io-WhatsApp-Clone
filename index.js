f4581c50fdbd5e2c328e6b9aa2725d8330a7a5a6
Manou <manouhaouzi@gmail.com>
2022-09-11 12:34:18 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const server = require("http").Server(app);
+const { Server } = require("socket.io");
+const port = 3000;
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", " https://cc1a-37-142-167-222.eu.ngrok.io/"]
+  }
+});
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
 
-http.listen(port, () => {
+let users = [];
+
+io.on("connection", (socket) => {
+  users.push({userName:       
+  socket.handshake.query.userName, userId: socket.id})
+  //io.emit('new user', { userId: socket.id })
+  io.emit('new user', users)
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    users = users.filter(function(e) { return e.userId !== socket.id })
+    console.log('nbUsersConnect: ', users.length);
+    io.emit('user disonnected', users)
+  })
+  socket.on('chat message', (msg) => {
+    //console.log("message: " + msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+  // forward the private message to the right recipient
+  socket.on("private message", ({content, to}) => {
+    console.log("to: ", to)
+    console.log("content: ", content);
+    try {
+          socket.to(to.userId).emit("private message", {
+      content,
+      from: socket.id,
+    }) 
+    } catch (error) {
+      console.error(error)
+    }
+
+  });
+  console.log('nbUsersConnect: ', users.length);
+  console.log('users: ', users)
+})
+
+server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
